c1d6b5fcf205cfc90ff51bd77288b3db64ea203b
euglin1130 <caoqijia1130@gmail.com>
2017-02-27 17:34:53 -0800

    Implemented test file loading

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,9 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     //setup the file path
@@ -13,6 +16,12 @@ public class App {
     private static File[] fileList;
     private static StringBuilder umlURL = new StringBuilder();
     private static String outputFile;
+    private List<String> varList;
+    private List<String> funcList;
+    private static final String spliter = ",";//splitting one relationship
+    private static final String c_ini = "[";//class ini
+    private static final String c_end = "]";//class end
+    private static final String c_interface = "<<interface>>";//interface
     public static void main(String[] args) throws Exception {
 
         filePath = "src/test/java/uml-parser-test-1";
@@ -21,9 +30,9 @@ public class App {
         outputFile = "test1.png";
         App obj = new App();
         obj.parse();
-        /*
+/*
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("src/test/java/Hello1.java");
+        FileInputStream in = new FileInputStream("src/test/java/uml-parser-test-1/A.java");
 
         // parse the file
         CompilationUnit cu = JavaParser.parse(in);
@@ -33,17 +42,32 @@ public class App {
 
         // visit and print the methods names
         new MethodVisitor().visit(cu, null);
-        */
+*/
         //Diagram generator
         umlURL.append("\"[A%7C-x:int;-y:int(*)]1-0..*[B],[A]-1[C],[A]-*[D]\"");
         ImgGenerator sample = new ImgGenerator(umlURL.toString());
         System.exit(1);
     }
-    public void parse(){
-        for(File file:fileList){
-            if(file.isFile()){
-                System.out.println(file.getName());
+
+    private void parse(){
+        try {
+            for(File file:fileList){
+                if(file.isFile()){
+                    if(umlURL.length() > 0){
+                        umlURL.append(spliter);
+                    }
+                    varList = new ArrayList<String>();
+                    funcList = new ArrayList<String>();
+                    // creates an input stream for the file to be parsed
+                    FileInputStream in = new FileInputStream(filePath + "/" + file.getName());
+                    // parse the file
+                    CompilationUnit cu = JavaParser.parse(in);
+                    System.out.println(cu.toString());
+
+                }
             }
+        }catch(Exception e) {
+            System.out.println(e);
         }
     };
     private static class MethodVisitor extends VoidVisitorAdapter<Void> {
