7fcddbb10d2f94baaa58c0e169309fd5e4622ad3
euglin1130 <caoqijia1130@gmail.com>
2017-02-28 16:19:53 -0800

    Implemented methodbuilder function

--- src/main/java/umlParser.java
+++ src/main/java/umlParser.java
@@ -3,6 +3,8 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.type.ReferenceType;
+import com.github.javaparser.ast.type.VoidType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
@@ -21,10 +23,12 @@ public class umlParser {
     private List<CompilationUnit> cuList;
     private List<String> classList;
     private List<String> interfaceList;
+    private List<String> methodList;
     private HashMap<String, List<String>> varList;
-    private HashMap<String, List<String>> funcList;
+
     private HashMap<String, List<String>> c_interMap;//class map to implements interface
     private HashMap<String, List<String>> c_superMap;//class map to extends super class
+    private boolean class_checker = false;
     private static final String spliter = ",";//splitting one relationship
     private static final String c_ini = "[";//class ini
     private static final String c_end = "]";//class end
@@ -41,29 +45,27 @@ public class umlParser {
         this.c_superMap = new HashMap<String, List<String>>();
         this.c_interMap = new HashMap<String, List<String>>();
 
-        umlURL.append("\"[A%7C-x:int;-y:int(*)]1-0..*[B],[A]-1[C],[A]-*[D]\"");
+        //umlURL.append("\"[A%7C-x:int;-y:int(*)]1-0..*[B],[A]-1[C],[A]-*[D]\"");
         ImgGenerator sample = new ImgGenerator(umlURL.toString());
     }
     public void parse(){
         try {
             for(File file:fileList){
                 if(file.isFile()){
+                    methodList = new ArrayList<String>();
                     // creates an input stream for the file to be parsed
-                    //System.out.println(filePath + "/" + file.getName());
                     FileInputStream in = new FileInputStream(filePath + "/" + file.getName());
                     CompilationUnit cu = JavaParser.parse(in);
                     in.close();
                     cuList.add(cu);
                     buildClassInterfaceList(cu);
+                    buildContents(cu);
                 }
-            }
-            for(CompilationUnit cu: cuList) {
-                buildClassContents(cu);
             }
         }catch(Exception e) {
             System.out.println(e);
         }
-    };
+    }
 
     //Build class/interface map of each cu
     public void buildClassInterfaceList(CompilationUnit cu){
@@ -73,8 +75,10 @@ public class umlParser {
                 ClassOrInterfaceDeclaration dec_checker = (ClassOrInterfaceDeclaration) c;
                 //build interface list
                 if(dec_checker.isInterface()){
+                    class_checker = false;
                     interfaceList.add(dec_checker.getName().toString());
                 }else{//build class list
+                    class_checker = true;
                     classList.add(dec_checker.getName().toString());
                     //class extends superclass
                     List<String> extendsList = new ArrayList<String>();
@@ -100,10 +104,72 @@ public class umlParser {
     }
 
     //Build class contents
-    public void buildClassContents(CompilationUnit cu){
-        for (Node child : cu.getChildNodes()){
+    public void buildContents(Node child){
+        //check looping
             if(child instanceof FieldDeclaration){
+                FieldDeclaration field = (FieldDeclaration) child;
+                String accessMod = "";
+                if(field.isPrivate()){
+                    accessMod = "-";
+                }else if(field.isPublic()){
+                    accessMod = "+";
+                }
+            }else if(child instanceof MethodDeclaration){
+                if(class_checker) {
+                    buildMethods(child);
+                }
+            }else if(child instanceof ConstructorDeclaration){
 
+            }
+            for (Node n : child.getChildNodes()){
+                //System.out.println(n);
+                buildContents(n);
+            }
+    }
+
+    public void buildMethods(Node node) {
+        MethodDeclaration method = (MethodDeclaration)node;
+        StringBuilder method_str = new StringBuilder();
+        String method_type = "";
+        String method_name = "";
+        String accessMod = "";
+
+        boolean access_checker = false;
+        if(method.isPrivate()){
+            accessMod = "-";
+        }else if(method.isPublic()){//static>?
+            accessMod = "+";
+            access_checker = true;
+        }
+        method_str.append(accessMod);
+
+        if (access_checker) {//if is class
+            //System.out.println(node);
+            method_type = method.getType().toString();
+            method_name = method.getName().toString();
+            method_str.append(method_name);
+
+            method_str.append("(");
+            for(Parameter para:method.getParameters()){
+                //pare is by pair like: A1 a1
+                String[] tmp = para.toString().split(" ");
+                if(tmp.length == 2) {
+                    method_str.append(tmp[1] + ":" + tmp[0]);
+                    method_str.append(",");
+                }
+            }
+            if(method.getParameters().isEmpty()){
+                method_str.append(")");
+            } else{
+                method_str.setLength(method_str.length() - 1);//remove last comma
+                method_str.append(")");
+            }
+            method_str.append(":" + method_type);
+            System.out.println(method_str);
+            if (method_name.startsWith("get") || method_name.startsWith("set")) {
+                //IGNORE
+            } else {
+                methodList.add(method_str.toString());
             }
         }
     }
